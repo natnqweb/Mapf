5727c2a805db34f21d7bbad812943272cb19b897
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2021-10-30 16:20:42 +0200

    Update Mapf.cpp

--- src/Mapf.cpp
+++ src/Mapf.cpp
@@ -5,7 +5,7 @@ double mapf(double x, double in_min, double in_max, double out_min, double out_m
 
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-double mapf_ADC(double value,double out_min,double out_max,uint8_t input_bit_resolution)
+double mapf_ADC(double value,double out_min,double out_max,unsigned char input_bit_resolution)
 {
 	double in_min=0;
 	double in_max=2<<input_bit_resolution-1;
