7ca018c1e61cac8f4396e28e9aa9fa3ed11117f0
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2021-10-30 16:20:27 +0200

    Update Mapf.h

--- src/Mapf.h
+++ src/Mapf.h
@@ -6,5 +6,5 @@
 double mapf(double x, double in_min=0, double in_max=1023, double out_min=0, double out_max=5);
 
 // mapf_bit will transform whatever resolution of analog read or your input is to provided  output range as out_min and out_max
-double mapf_ADC(double value,double out_min=0,double out_max=5,uint8_t input_bit_resolution=10);
+double mapf_ADC(double value,double out_min=0,double out_max=5,unsigned char input_bit_resolution=10);
 #endif
